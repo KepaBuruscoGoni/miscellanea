27293bacefed92a2635a1b2064985b7a975fb819
KepaBuruscoGoni <125286397+KepaBuruscoGoni@users.noreply.github.com>
2023-10-25 11:42:53 +0100

    Argument parsing
    Examples of command line argument parsing

---
+++ command_line_parser_example01.py
@@ -0,0 +1,27 @@
+## -*- coding: utf-8 -*-
+"""
+@author: kburusco
+@date: 25/10/2023
+"""
+
+import sys
+
+
+def main():
+
+    print("\n=> Script to read arguments from command line", flush=True)
+
+    name = sys.argv[1]
+    surename = sys.argv[2]
+    age = sys.argv[3]
+    active = sys.argv[4]
+
+    print(f"\n   My friend {name} {surename} is {age} and prefers being active in the {active}")
+
+    print("\n=> Normal Termination\n", flush=True)
+
+    sys.exit()
+
+
+if __name__ == '__main__':
+        main()
---
+++ command_line_parser_example02.py
@@ -0,0 +1,79 @@
+## -*- coding: utf-8 -*-
+"""
+@author: kburusco
+@date: 25/10/2023
+"""
+
+import sys
+import argparse
+
+
+def main():
+
+    print("\n=> Script to read arguments from command line", flush=True)
+
+    args = argument_parser()
+
+    name = args.name
+    surename = args.surename
+    age = args.age
+    active = args.active
+
+    print(f"\n   My friend {name} {surename} is {age} and prefers being active in the {active}")
+
+    print("\n=> Normal Termination\n", flush=True)
+
+    sys.exit()
+
+
+'''
+###############################################
+#                FUNCTION
+###############################################
+'''
+
+
+def argument_parser():
+    argparser = argparse.ArgumentParser(
+        description='Script to read arguments from command line')
+
+    argparser.add_argument(
+        '-name',
+        '--name',
+        help='User name [name=Anna]',
+        type=str,
+        default='Anna'
+    )
+
+    argparser.add_argument(
+        '-surename',
+        '--surename',
+        help='User surename [surename=Williams]',
+        type=str,
+        default='Williams'
+    )
+
+    argparser.add_argument(
+        '-age',
+        '--age',
+        help='User age [age=35]',
+        type=int,
+        default=35
+    )
+
+    argparser.add_argument(
+        '-active',
+        '--active',
+        help='Run in serial or in parallel? [parallel=yes]',
+        type=str,
+        choices=['morning', 'evening'],
+        default='evening'
+    )
+
+    arguments = argparser.parse_args()
+
+    return arguments
+
+
+if __name__ == '__main__':
+    main()
