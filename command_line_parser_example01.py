c9ac193befa596c8d0220fc867b06824f68f5f59
KepaBuruscoGoni <125286397+KepaBuruscoGoni@users.noreply.github.com>
2023-10-25 11:53:42 +0100

    Added input command line
    Added some extra lines with examples: input command line and script name.

--- command_line_parser_example01.py
+++ command_line_parser_example01.py
@@ -11,13 +11,15 @@ def main():
 
     print("\n=> Script to read arguments from command line", flush=True)
 
+    script = sys.argv[0]
     name = sys.argv[1]
     surename = sys.argv[2]
     age = sys.argv[3]
     active = sys.argv[4]
 
+    print(f"\n   The input command line is: {' '.join(sys.argv)}")
+    print(f"\n   The script name is {script}")
     print(f"\n   My friend {name} {surename} is {age} and prefers being active in the {active}")
-
     print("\n=> Normal Termination\n", flush=True)
 
     sys.exit()
